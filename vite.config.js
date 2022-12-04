4a49250fe4809222a910f6bd19fca72258da7f12
JoaqPareja <80791761+JoaqPareja@users.noreply.github.com>
2022-12-04 10:12:50 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "www.https://talleresdyg.com/",
+  base: "/talleresdyg.com/",
   plugins: [react()],
   overlay: {
     warnings: false,
