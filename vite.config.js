a00663f8ad4585e2df31c8ce02e99dfc8b807d5a
JoaqPareja <80791761+JoaqPareja@users.noreply.github.com>
2022-12-05 22:07:28 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "joaqpareja.github.io/TallerProject/",
+  base: "/",
   plugins: [react()],
   overlay: {
     warnings: false,
